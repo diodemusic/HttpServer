37cb540965a950455d55d4d7d52cf0516fc60d83
saves <diodemusicuk@gmail.com>
2025-02-18 04:14:08 +0000

    Create server class

--- src/main.py
+++ src/main.py
@@ -1,35 +1,37 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-import time
 
 
-HOST = "localhost"
-PORT = 9999
+class SavesServer(BaseHTTPRequestHandler):
+    def get_html(self):
+        with open("content/index.html", "r") as f:
+            return f.read()
 
-
-class HTTP(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
+        self.send_header("Content-Type", "text/html")
         self.end_headers()
 
-        html = "<html><body>Hello World.</body></html>"
+        html = self.get_html()
         self.wfile.write(bytes(html, "utf-8"))
 
-    def do_POST(self):
-        self.send_response(200)
-        self.send_header("Content-tpye", "application/json")
-        self.end_headers()
 
-        date: str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
-        date_json: str = '{"time": "' + date + '"}'
-        self.wfile.write(bytes(date_json, "utf-8"))
+class Server(BaseHTTPRequestHandler):
+    server = None
+    html = ""
+
+    def __init__(self, address, port):
+        self.address = address
+        self.port = port
+
+    def run(self):
+        self.server = HTTPServer((self.address, self.port), SavesServer)
+        print(f"Server running on {self.address}:{self.port}")
+        self.server.serve_forever()
+
+    def stop(self):
+        self.server.server_close()
 
 
-server = HTTPServer((HOST, PORT), HTTP)
-
-print(f"Server running: {HOST}:{PORT}")
-
-server.serve_forever()
-server.server_close()
-
-print("Server stopped.")
+server = Server("localhost", 9999)
+server.run()
+server.stop()
